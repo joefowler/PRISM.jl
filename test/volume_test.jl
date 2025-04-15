6d39561a7e5fc59207e02c5774196a8b91028f7c
Joe Fowler <drjoefowler@gmail.com>
2025-04-15 16:13:01 -0600

    Add some tests of `path_integrated_density()`

--- test/volume_test.jl
+++ test/volume_test.jl
@@ -1,19 +1,32 @@
 using PRISM
+using LinearAlgebra
 
 @testset "volume_object" begin
-    @testset "contructors" begin
-        nx, ny, nz = 3, 5, 4
-        edges = [0:nx, 0:ny, 0:nz]
-        v1 = Volume(edges, zeros(Float64, nx, ny, nz), [nx, ny, nz], nx*ny*nz)
-        v2 = Volume(edges)
-        @test all(v2.densities .== 0.0)
-        @test v1.voxelsTotal == v2.voxelsTotal
-        @test all(v1.voxelsPerEdge .== v2.voxelsPerEdge)
-    end
     nx, ny, nz = 3, 5, 4
     edges = [0:nx, 0:ny, 0:nz]
     v = Volume(edges)
+    
+    @testset "contructors" begin
+        v1 = Volume(edges, zeros(Float64, nx, ny, nz), [nx, ny, nz], nx*ny*nz)
+        @test all(v.densities .== 0.0)
+        @test v1.voxelsTotal == v.voxelsTotal
+        @test all(v1.voxelsPerEdge .== v.voxelsPerEdge)
+    end
+
     @test PRISM.corners(v) == [[0,nx],[0,ny], [0,nz]]
     p = [1.5, 2.5, 1.3]
     @test all(PRISM.voxelScale(v, p) .≈ p)
+
+    # With a random pattern, check that z-aligned paths agree
+    v.densities[:,:,:] = rand(nx, ny, nz)
+    @test PRISM.path_integrated_density(v, [1.5, 1.5, 0], [1.5, 1.5, nz]) ≈ sum(v.densities[2, 2, :])
+    @test PRISM.path_integrated_density(v, [1.5, 3.5, 0], [1.5, 3.5, nz]) ≈ sum(v.densities[2, 4, :])
+
+    # Now check a few random positions where the line goes from one y-plane pixel edge to the next
+    Ntests = 5
+    for (x,y) in zip(nx*rand(Ntests), rand(1:ny, Ntests))
+        p3 = [x, y-1, 0]
+        p4 = [x, y, nz]
+        @test PRISM.path_integrated_density(v, p3, p4) ≈ sum(v.densities[1+Int(floor(x)), y, :]) * norm(p4-p3)/nz
+    end
 end
